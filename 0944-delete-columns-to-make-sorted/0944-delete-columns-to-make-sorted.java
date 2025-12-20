0d91b6f67c7a89a9912f6388294a5d7615c10da0
Mahak Goswami <mahak.goswami2050@gmail.com>
2025-12-20 14:43:34 +0530

    Time: 8 ms (74.4%), Space: 47.2 MB (13.28%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,14 +1,22 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        HashSet<Integer> set=new HashSet<>();
+        // HashSet<Integer> set=new HashSet<>();
         int count=0;
-        for(int i=1; i<strs.length; i++){
-            for(int j=0; j<strs[i].length(); j++){
-                String before=strs[i-1];
-                String curr=strs[i];
-                if(before.charAt(j)>curr.charAt(j) && !set.contains(j)){
-                    set.add(j);
+        // for(int i=1; i<strs.length; i++){
+        //     for(int j=0; j<strs[i].length(); j++){
+        //         String before=strs[i-1];
+        //         String curr=strs[i];
+        //         if(before.charAt(j)>curr.charAt(j) && !set.contains(j)){
+        //             set.add(j);
+        //             count++;
+        //         }
+        //     }
+        // }
+        for(int i=0; i<strs[0].length(); i++){
+            for(int j=1; j<strs.length; j++){
+                if(strs[j].charAt(i)<strs[j-1].charAt(i)){
                     count++;
+                    break;
                 }
             }
         }
