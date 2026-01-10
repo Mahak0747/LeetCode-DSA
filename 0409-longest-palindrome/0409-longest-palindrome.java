49c08f22ceadaa4b8223c1fd8350c8bd6f3b4b0c
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-01-10 23:47:10 +0530

    Time: 1 ms (99.88%), Space: 43.2 MB (44.4%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] arr = new int[128]; 
+        for (char c : s.toCharArray()) {
+            arr[c]++;
+        }
+        int ans=0;
+        boolean is=false;
+        for (int count : arr) {
+            ans += (count / 2) * 2; // add pairs
+            if (count % 2 == 1) {
+                is = true;
+            }
+        }
+        if(is)ans++;
+        return ans;
+    }
+}
