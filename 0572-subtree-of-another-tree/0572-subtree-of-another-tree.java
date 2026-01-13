e24a5468947a5fef8e9b9523693bf13cb9012569
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-01-13 23:07:01 +0530

    Time: 2 ms (99.05%), Space: 46.4 MB (74.84%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,14 +14,14 @@
  * }
  */
 class Solution {
-    private boolean ident(TreeNode root, TreeNode subRoot) {
-        if(root==null && subRoot==null)return true;
-        if(root==null || subRoot==null || root.val!=subRoot.val)return false;
-        return ident(root.left,subRoot.left)&&ident(root.right,subRoot.right);
-    }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root==null)return false;
         if(ident(root,subRoot))return true;
         return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
+    }
+    private boolean ident(TreeNode root, TreeNode subRoot) {
+        if(root==null && subRoot==null)return true;
+        if(root==null || subRoot==null || root.val!=subRoot.val)return false;
+        return ident(root.left,subRoot.left)&&ident(root.right,subRoot.right);
     }
 }
