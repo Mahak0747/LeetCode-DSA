63c085dafcaa9ed39a91f21df5f8b453bd8dc084
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-01-23 01:57:42 +0530

    Time: 0 ms (100%), Space: 42.5 MB (9.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,12 @@
 class Solution {
+    private static int dp(int[] fib,int n){
+        if(n==0||n==1)return n;
+        if(fib[n]!=0)return fib[n];
+        fib[n]=dp(fib,n-1)+dp(fib,n-2);
+        return fib[n];
+    }
     public int fib(int n) {
-        if(n==0 || n==1){
-            return n;
-        }
-        return fib(n-2)+fib(n-1); 
+        int[] arr=new int[n+1];
+        return dp(arr,n);
     }
 }
