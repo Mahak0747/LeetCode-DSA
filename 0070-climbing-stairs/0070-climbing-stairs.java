ee6493b9f64bf56b11a3274e7abc2417a84925c1
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-01-23 03:03:53 +0530

    Time: 0 ms (100%), Space: 42.1 MB (62.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,13 @@
 class Solution {
+    private static int dp(int[] steps,int n){
+        if(n==0)return 1;
+        if(n<0)return 0;
+        if(steps[n]!=0)return steps[n];
+        steps[n]=dp(steps,n-1)+dp(steps,n-2);
+        return steps[n];
+    }
     public int climbStairs(int n) {
-        if(n==0||n==1)return 1;
-        return climbStairs(n-1)+climbStairs(n-2);
+        int[] arr=new int[n+1];
+        return dp(arr,n);
     }
 }
