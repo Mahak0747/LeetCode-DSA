1126eaf3702f0bf78fe4fc233eeddbe69d012784
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-01-29 23:36:41 +0530

    Time: 1 ms (45.39%), Space: 47 MB (59.94%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,23 +14,33 @@
  * }
  */
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null) {
-            return 0;
+    static class Info{
+        int dia;
+        int hig;
+        public Info(int d,int h){
+            this.dia=d;
+            this.hig=h;
         }
-        int dFromLeft = diameterOfBinaryTree(root.left);
-        int dFromRight = diameterOfBinaryTree(root.right);
-        int l = height(root.left);
-        int r = height(root.right);
-        int dFromRoot = l + r;
-        return Math.max(dFromRoot, Math.max(dFromLeft, dFromRight));
     }
-    private int height(TreeNode root){
-        if (root == null) {
-            return 0;
-        }
-        int l = height(root.left);
-        int r = height(root.right);
-        return Math.max(l, r)+1;
+    public int diameterOfBinaryTree(TreeNode root) {
+        return diameter(root).dia;
+        // if (root == null) {
+        //     return 0;
+        // }
+        // int dFromLeft = diameterOfBinaryTree(root.left);
+        // int dFromRight = diameterOfBinaryTree(root.right);
+        // int l = height(root.left);
+        // int r = height(root.right);
+        // int dFromRoot = l + r;
+        // return Math.max(dFromRoot, Math.max(dFromLeft, dFromRight));
+    }
+    private Info diameter(TreeNode root){
+        if (root == null)return new Info(0,-1);
+        Info l = diameter(root.left);
+        Info r = diameter(root.right);
+        int h=Math.max(l.hig,r.hig)+1;
+        int diaFromRoot=l.hig+r.hig+2;
+        int d=Math.max(diaFromRoot,Math.max(l.dia,r.dia));
+        return new Info(d,h);
     }
 }
