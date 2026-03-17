beac97a454d964a17c1120db2c57ba32df641728
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-03-17 19:46:47 +0530

    Time: 2 ms (99.74%), Space: 53.6 MB (79.89%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,20 +1,13 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int m = trust.length;
-        boolean[]a = new boolean[n];
-        int[]cnt = new int[n];
-        for(int i = 0;i<m;i++){
-            a[trust[i][0]-1]=true;
-            cnt[trust[i][1]-1]++;
+        int[] dfs = new int[n + 1];
+        for (int[] t : trust) {
+            dfs[t[0]]--; 
+            dfs[t[1]]++; 
+        }        
+        for (int i = 1; i <= n; i++) {
+            if (dfs[i] == n - 1) return i;
         }
-        int ans = -1;
-        for(int i = 0;i<n;i++){
-            if(!a[i] && cnt[i]==n-1){
-                ans=i+1;
-                break;
-            }
-        }
-        return ans;
-
+        return -1;
     }
 }
