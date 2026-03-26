01d969aeac1a3b148125cb7af39ed0cc3353c2ed
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-03-27 02:09:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (81.95%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(goal.length()!=s.length())return false;
+        s+=s;
+        return s.contains(goal);
+    }
+}
