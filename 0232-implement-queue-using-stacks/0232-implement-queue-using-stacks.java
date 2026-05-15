7ed656510b6e65a9a37756f0766754fa1933bb1b
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-05-15 18:07:00 +0530

    Time: 0 ms (100%), Space: 42.8 MB (38.8%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,46 +1,45 @@
 import java.util.*;
+
 class MyQueue {
-    Stack<Integer> s1=new Stack<>();
-    Stack<Integer> s2=new Stack<>();
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
 
     public MyQueue() {
-        
+
     }
-    
+
     public void push(int x) {
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
-        }
         s1.push(x);
-        while(!s2.isEmpty()){
-            s1.push(s2.pop());
-        }
     }
-    
+
     public int pop() {
-        if(empty()){
-            return -1;
+
+        if (empty()) return -1;
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
         }
-        return s1.pop();
+
+        return s2.pop();
     }
-    
+
     public int peek() {
-        if(empty()){
-            return -1;
+
+        if (empty()) return -1;
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
         }
-        return s1.peek();
+
+        return s2.peek();
     }
-    
+
     public boolean empty() {
-        return s1.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
