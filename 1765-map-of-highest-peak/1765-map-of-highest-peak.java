19ae0ae8f00964da2cb68c1ebc2907817253da58
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-06-04 00:08:03 +0530

    Time: 41 ms (88.66%), Space: 184.4 MB (26.1%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -0,0 +1,41 @@
+class Pair{
+    int i;
+    int j;
+    public Pair(int i, int j){
+        this.i=i;
+        this.j=j;
+    }
+}
+class Solution {
+    public int[][] highestPeak(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] ans=new int[n][m];
+        Queue<Pair> q=new LinkedList<Pair>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                ans[i][j]=-1;
+                if(grid[i][j]==1){
+                    ans[i][j]=0;
+                    q.add(new Pair(i,j));
+                }
+            }
+        }
+        int[] row={-1,0,1,0};
+        int[] col={0,1,0,-1};
+        while(!q.isEmpty()){
+            Pair curr=q.remove();
+            int r=curr.i;
+            int c=curr.j;
+            for(int i=0; i<4; i++){
+                int nr=r+row[i];
+                int nc=c+col[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && ans[nr][nc]==-1){
+                    q.add(new Pair(nr,nc));
+                    ans[nr][nc]=ans[r][c]+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
