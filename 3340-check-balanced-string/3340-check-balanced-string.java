5cd1da39a30402af51fa7ae7e01df4d4974a17f9
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-07-05 06:33:17 +0530

    Time: 1 ms (98.54%), Space: 43.5 MB (64.24%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int odd=0;
+        int even=0;
+        for(int i=0; i<num.length(); i++){
+            if(i%2==0)odd+=(num.charAt(i)-'0');
+            else even+=(num.charAt(i)-'0');
+        }
+        return odd==even;
+    }
+}
