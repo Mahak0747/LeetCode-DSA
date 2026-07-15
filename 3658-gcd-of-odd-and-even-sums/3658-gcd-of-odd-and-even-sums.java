6e26aa0438126ee1a8b013b711931c3b9c17eb94
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-07-15 10:25:13 +0530

    Time: 0 ms (100%), Space: 42.6 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,17 +1,5 @@
 class Solution {
-    private static int gcd(int a,int b){
-        if (b == 0) {
-            return Math.abs(a);
-        }
-        return gcd(b, a % b);
-    }
     public int gcdOfOddEvenSums(int n) {
-        int os=0;
-        int es=0;
-        for(int i=0; i<=2*n; i++){
-            if(i%2==0)es+=i;
-            else os+=i;
-        }
-        return gcd(os,es);
+        return n;
     }
 }
