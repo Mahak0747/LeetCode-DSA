1aafe92674d65bd82b622ecb2078e9af3c3cd5d9
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-08-05 04:57:48 +0530

    Time: 0 ms (100%), Space: 46.8 MB (43.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,37 +1,22 @@
-class Pair{
-    int f;
-    int s;
-    public Pair(int f, int s){
-        this.f=f;
-        this.s=s;
-    }
-}
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int val=image[sr][sc];
-        if (val == color) return image;
-        int m=image.length;
-        int n=image[0].length;
-        boolean[][] seen=new boolean[m][n];
-        Queue<Pair> q=new LinkedList<Pair>();
-        q.add(new Pair(sr,sc));
-        seen[sr][sc]=true;
-        int[] row={-1,1,0,0};
-        int[] col={0,0,-1,1};
-        while(!q.isEmpty()){
-            int r=q.peek().f;
-            int c=q.peek().s;
-            q.remove();
-            image[r][c]=color;
-            for (int k = 0; k < 4; k++){
-                int nr=r+row[k];
-                int nc=c+col[k];
-                if(nr>=0 && nc>=0 && nr<m && nc<n && image[nr][nc]==val && !seen[nr][nc]){
-                    seen[nr][nc]=true;
-                    q.add(new Pair(nr,nc));
-                }
+    private static void dfs(int r,int c,int val, int color, int[][] ans, int[][] image, int[] row, int[] col){
+        ans[r][c]=color;
+        int n=image.length;
+        int m=image[0].length;
+        for(int i=0; i<4; i++){
+            int nrow=r+row[i];
+            int ncol=c+col[i];
+            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && image[nrow][ncol]==val && ans[nrow][ncol]!=color){
+                dfs(nrow,ncol,val,color,ans,image,row,col);
             }
         }
-        return image;
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[][] ans=image;
+        int val=image[sr][sc];
+        int[] row={-1,0,1,0};
+        int[] col={0,1,0,-1};
+        dfs(sr,sc,val,color,ans,image,row,col);
+        return ans;
     }
 }
