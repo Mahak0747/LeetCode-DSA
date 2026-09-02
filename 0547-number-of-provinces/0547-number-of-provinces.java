48d1244b4d9654620a912188af160d964b3bb48e
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-09-02 10:06:17 +0530

    Time: 2 ms (41.51%), Space: 46.4 MB (99.82%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,20 +1,38 @@
-class Solution {
-    private static void findCircleNumUtil(int[][] graph,int idx, boolean seen[]){
-        seen[idx]=true;
-        for(int i=0; i<graph.length; i++){
-            if(graph[idx][i]==1&&!seen[i]){
-                findCircleNumUtil(graph,i,seen);
-            }
+class DisjointSet{
+    int[] parent;
+    public DisjointSet(int n){
+        parent=new int[n];
+        for(int i=0; i<n; i++){
+            parent[i]=i;
         }
     }
-    public int findCircleNum(int[][] graph) {
-        int c=0;
-        boolean seen[]=new boolean[graph.length];
-        for(int i=0; i<graph.length; i++){
-            if(!seen[i]){
-                findCircleNumUtil(graph,i,seen);
-                c++;
+    public int findParent(int a){
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = findParent(parent[a]);
+    }
+    public void unionOfSets(int a,int b){
+        int pa = findParent(a);
+        int pb = findParent(b);
+        if (pa == pb) {
+            return;
+        }
+        parent[pb] = pa;
+    }
+}
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        DisjointSet ds=new DisjointSet(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j]==1)ds.unionOfSets(i,j);
             }
+        }
+        int c=0;
+        for(int i=0; i<n; i++){
+            if(ds.parent[i]==i)c++;
         }
         return c;
     }
