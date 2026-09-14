afd6448300aa233a27afea675c4388dbb39ef00f
Mahak Goswami <mahak.goswami2050@gmail.com>
2026-09-15 00:01:33 +0530

    Time: 1 ms (16.9%), Space: 43.7 MB (11.74%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,21 @@
 class Solution {
-    private static int[][] dp;
-    private static int countMazeUtil(int[][] maze,int r, int c){
-        if (!isSafe(maze, r, c)) return 0;
-        if(r==maze.length-1&&c==maze[0].length-1)return 1;
-        if (dp[r][c] != -1) return dp[r][c];
-        dp[r][c] = countMazeUtil(maze, r + 1, c) + countMazeUtil(maze, r, c + 1);
-        return dp[r][c];
-    }
-    private static boolean isSafe(int[][] maze,int r, int c){
-        return r>=0 && c>=0 && r<maze.length && c<maze[0].length && maze[r][c]==0;
-    }
-    public int uniquePathsWithObstacles(int[][] maze) {
-        int N = maze.length;
-        int M = maze[0].length;
-        dp = new int[N][M];
-        for (int i = 0; i < N; i++)
-            Arrays.fill(dp[i], -1);
-        if (maze[0][0] == 1 || maze[N-1][M-1] == 1) return 0;
-        return countMazeUtil(maze, 0, 0);
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        int[][] dp=new int[m][n];
+        dp[0][0]=1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int r=0;
+                int d=0;
+                if(obstacleGrid[i][j]==1)dp[i][j]=0;
+                else{
+                    if(i>0)r=dp[i-1][j];
+                    if(j>0)d=dp[i][j-1];
+                    if (i != 0 || j != 0)dp[i][j]=r+d;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
